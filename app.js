b7abdad5e1960bcccd9fed62bafab1aeb6b8343d
Sam Koh <sam910615@gmail.com>
2022-07-27 22:35:06 +0800

    Change port coding

--- app.js
+++ app.js
@@ -211,11 +211,14 @@ app.get("/:customListName", function (req, res) {
 
 
 //Port to database and localhost
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 3000;
-}
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//     port = 3000;
+// }
 
-app.listen(port, function () {
-    console.log("Server started successfully");
+// app.listen(port, function () {
+//     console.log("Server started successfully");
+// });
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
